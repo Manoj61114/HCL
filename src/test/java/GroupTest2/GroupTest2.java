5f8c64fba79c85d43c9b2847b0d91b63e4e2c5dd
Manoj61114 <mannu61114@gmail.com>
2021-09-07 20:47:12 +0530

    added RS PJ

--- src/test/java/GroupTest2/GroupTest2.java
+++ src/test/java/GroupTest2/GroupTest2.java
@@ -5,12 +5,6 @@ import org.testng.annotations.Test;
 public class GroupTest2 {
 
 	
-	@Test(groups= {"FunctionalTest"})
-	public void mainTest() {
-		System.out.println("MainTest GroupTest2 Class");
-	}
-	
-
 	@Test(groups= {"Smoke","Sanity"})
 	public void mainTest1() {
 		System.out.println("MainTest1 GroupTest2 Class");
