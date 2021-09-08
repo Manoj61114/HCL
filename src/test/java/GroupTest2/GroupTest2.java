da4e16ca7964b4b0e0c831fc331c34fbf8ca1b63
Manoj61114 <mannu61114@gmail.com>
2021-09-08 15:12:54 +0530

    RS added

--- src/test/java/GroupTest2/GroupTest2.java
+++ src/test/java/GroupTest2/GroupTest2.java
@@ -7,7 +7,14 @@ public class GroupTest2 {
 	
 	@Test(groups= {"Smoke","Sanity"})
 	public void mainTest1() {
-		System.out.println("MainTest1 GroupTest2 Class");
+		System.out.println("MainTest2 GroupTest2 Class");
 	}
+	
+	@Test(groups= {"Smoke","Sanity"})
+	public void mainTest2() {
+		System.out.println("MainTest2 GroupTest2 Class");
+	}
+	
+	
 	
 }
